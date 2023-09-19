043eaf54b057a079cdffad71691cf835c74732df
DidierDefrance <didier@d-defrance.fr>
2023-09-19 11:43:01 +0200

    scripts h2

---
+++ tp/appliSpringSansSpringBoot/src/script/h2/create_h2_database.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+call set_env.bat
+java -classpath %H2_CLASSPATH% org.h2.tools.RunScript -url %MY_H2_DB_URL%  -user sa -script init_db.sql -showResults
+pause
---
+++ tp/appliSpringSansSpringBoot/src/script/h2/set_env.bat
@@ -0,0 +1,11 @@
+
+REM set MVN_REPOSITORY=C:\ext\mvn-repository
+set MVN_REPOSITORY=C:\Users\d2fde\.m2\repository
+REM set MVN_REPOSITORY=C:\Users\formation\.m2\repository
+
+set MY_H2_DB_URL=jdbc:h2:~/mydbbank
+
+set PATH="C:\Prog\java\eclipse_jee_2023_06\eclipse\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_17.0.7.v20230425-1502\jre\bin"
+
+set H2_VERSION=2.1.214
+set H2_CLASSPATH=%MVN_REPOSITORY%\com\h2database\h2\%H2_VERSION%\h2-%H2_VERSION%.jar
