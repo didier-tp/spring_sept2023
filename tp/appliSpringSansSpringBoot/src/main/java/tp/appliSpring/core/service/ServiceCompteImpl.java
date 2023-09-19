e7fd5e570bc7dc0bc7d84cbec856c4dca7b25c76
DidierDefrance <didier@d-defrance.fr>
2023-09-19 15:58:44 +0200

    s

--- tp/appliSpringSansSpringBoot/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
+++ tp/appliSpringSansSpringBoot/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
@@ -3,6 +3,7 @@ package tp.appliSpring.core.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import tp.appliSpring.core.dao.DaoCompte;
 import tp.appliSpring.core.entity.Compte;
@@ -15,7 +16,7 @@ public class ServiceCompteImpl implements ServiceCompte {
 	@Qualifier("jpa")
 	private DaoCompte daoCompte;
 
-	//@Transactional
+	@Transactional
 	public void transferer(double montant, long numCptDeb, long numCptCred) {
 		Compte cptDeb = daoCompte.findById(numCptDeb);
 		cptDeb.setSolde(cptDeb.getSolde() - montant);
