856612e1063f4708cc97bdb89d7109410d07bc88
DidierDefrance <didier@d-defrance.fr>
2023-09-20 09:43:59 +0200

    debut appliSpring (springBoot)

---
+++ tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -0,0 +1,14 @@
+package tp.appliSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliSpringApplication.class, args);
+		System.out.println("http://localhost:8181/appliSpring");
+	}
+
+}
