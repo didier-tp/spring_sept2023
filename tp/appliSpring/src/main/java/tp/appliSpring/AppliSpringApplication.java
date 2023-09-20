c13831f8896ceca7baf1a079d4e81bfe8ccf8498
DidierDefrance <didier@d-defrance.fr>
2023-09-20 10:39:35 +0200

    avec profiles dev et prod

--- tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -5,9 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppliSpringApplication {
+	
+	public static void initProfiles() {
+		//java .... -Dspring.profiles.active=reInit,dev
+		String profilsActifs  = System.getProperty("spring.profiles.active");
+		if(profilsActifs==null || profilsActifs.equals("")) {
+			//valeur par défaut si pas encore précisé
+			System.setProperty("spring.profiles.active", "dev");
+			//System.setProperty("spring.profiles.active", "prod");
+		}
+	}
 
 	public static void main(String[] args) {
+		initProfiles();
 		SpringApplication.run(AppliSpringApplication.class, args);
+		//url de l'appli
 		System.out.println("http://localhost:8181/appliSpring");
 	}
 
